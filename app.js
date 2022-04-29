e36cdf20a0d70cdb370c86a7290e0454ea0cdc96
tm_ahad <tm.tahmid@gmail.com>
2022-04-29 13:19:08 +0600

    connection to server done

---
+++ app.js
@@ -0,0 +1,7 @@
+//const express = require('express') 
+import express from 'express' 
+const app = express() 
+const port = process.env.PORT || '3000' 
+app.listen(port, () => { 
+    console.log(`Server listening at http://localhost:${port}`)
+})
